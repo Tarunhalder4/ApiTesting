962e728c8505b7509ba11208510c192f5f7d4166
TarunHalder4 <tarunhalder9@gmail.com>
2023-12-18 10:48:05 +0530

    delete all file

--- app/src/main/java/com/example/apitesting/ApiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.apitesting
-
-sealed class ApiState{
-    object Loading:ApiState()
-    class Success(val posts:List<Post>):ApiState()
-    class Error(val massage:String):ApiState()
-    object Empty : ApiState()
-
-}
--- app/src/main/java/com/example/apitesting/MainActivity.kt
+++ app/src/main/java/com/example/apitesting/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.example.apitesting
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.apitesting.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,7 +9,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val postViewModel:PostViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,37 +17,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        binding.rec.layoutManager = LinearLayoutManager(this)
-        postViewModel.getPost()
-
-        lifecycleScope.launchWhenCreated {
-            postViewModel.stateFlow.collect {
-
-                when (it) {
-                    is ApiState.Loading -> {
-                        Log.d("tarun", "onCreate: loading")
-                    }
-
-                    is ApiState.Success -> {
-                        Log.d("tarun", "onCreate 12: ${it.posts}")
-                        val posts = it.posts
-                        val adapter = PostAdapter(this@MainActivity,posts)
-                        binding.rec.adapter = adapter
-
-                    }
-
-                    is ApiState.Error -> {
-                        Log.d("tarun", "onCreate: ${it.massage}")
-                    }
-
-                    else -> {
-                        Log.d("tarun", "onCreate: unwanted option")
-                    }
-                }
-            }
         }
 
 
 
-    }
+
 }
